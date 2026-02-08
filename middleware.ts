6de0c3cf47ee294f975a89f558fd984337cc63a3
Adhil-Akbar <356adhil@gmail.com>
2026-02-08 14:34:13 +0300

    Exclude HTML and YAML file extensions from middleware processing.

--- middleware.ts
+++ middleware.ts
@@ -74,6 +74,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * - public files (public directory)
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|html|yaml)$).*)',
     ],
 };
